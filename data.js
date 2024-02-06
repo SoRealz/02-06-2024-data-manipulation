d94fd717a5c4142ce97be88ecd938220552d2d08
Jacob Sorrell <jacoblsorrell1991@gmail.com>
2024-02-06 09:10:06 -0600

    commit

--- data.js
+++ data.js
@@ -6,7 +6,19 @@ const n2 = 15;
 const n3 = 20;
 const n4 = 5;
 
+// console.log(10+15+20+5)   
+const divisibleBy5 = (n1%5) + (n2%5) + (n3%5) + (n4%5) == 0;
+// console.log(divisibleBy5)
+// const isSum50 = (==)
 // Check one: add up to 50
+
+const arithmiticCHain = ((n2-n1) * (n3))%n4 == 0;
+// console.log(arithmiticCHain)
+
+const isLargerThan = n1 > n4;
+// console.log(isLargerThan)
+
+
 // This is a fairly simple operation using
 // arithmetic operators and a comparison.
 const isSum50 = (n1 + n2 + n3 + n4) == 50;
@@ -22,6 +34,9 @@ const isTwoOdd = (n1 % 2) + (n2 % 2) + (n3 % 2) + (n4 % 2) >= 2;
 // if ANY of the numbers is larger than 25.
 const isOver25 = n1 > 25 || n2 > 25 || n3 > 25 || n4 > 25;
 
+const isLessThan25 = n1 < 25 || n2 < 25 || n3 < 25 || n4 < 25;
+// console.log()
+
 // Check four: all unique numbers
 // This is long, and there are more efficient
 // ways of handling it with other data structures
@@ -32,10 +47,11 @@ const isUnique = n1 != n2 && n1 != n3 && n1 != n4 && n2 != n3 && n2 != n4 && n3 
 // for convenience. Note how we negate isOver25 using
 // the ! operator. We could also have tested for 
 // "isUnder25" as an alternative.
-const isValid = isSum50 && isTwoOdd && !isOver25 && isUnique;
+const isValid = isSum50 && isTwoOdd && isLessThan25 && isUnique;
 
 // Finally, log the results.
-console.log(isValid);
+// console.log(isValid);
+// console.log (`The result is : ${isValid}.`) 
 
 // Here's another example of how this COULD be done,
 // but it SHOULD NOT be done this way. As programmers,
